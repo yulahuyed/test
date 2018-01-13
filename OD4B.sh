dc3bfa1dda559becbaf80d123b59451ffeb03aef
yulahuyed <yulahuyed@vpsorg.top>
2018-01-13 13:59:04 +0800

    Update OD4B.sh

--- OD4B.sh
+++ OD4B.sh
@@ -65,6 +65,7 @@ sudo apt-get update && sudo apt-get install -y davfs2
 sudo chmod 777 /etc/davfs2/davfs2.conf
 echo " " >> /etc/davfs2/davfs2.conf
 echo " " >> /etc/davfs2/davfs2.conf
+echo "use_locks 0" >> /etc/davfs2/davfs2.conf
 echo "[${MPATH}]" >> /etc/davfs2/davfs2.conf
 echo "add_header Cookie rtFa=${rtFa};FedAuth=${FedAuth}" >> /etc/davfs2/davfs2.conf
 sudo /sbin/mount.davfs ${OD4B} ${MPATH}
