b824dbf38432d1afc3cdb691da73009eefc0e655
yulahuyed <yulahuyed@vpsorg.top>
2018-01-13 20:18:21 +0800

    Update gv.sh

--- gv.sh
+++ gv.sh
@@ -13,7 +13,7 @@ clear
 echo "$keyerrorlogo";
 echo " "
 
-echo "Please Input the Phone Number, e.g.3859998880"
+echo "Please Input the Phone Number, e.g. 3859998880"
 read gv
 echo " "
 echo " "
@@ -25,7 +25,7 @@ echo " "
 echo " "
 echo " "
 
-echo "Please Input your Curl Command:"
+echo "Please Input your Curl Command, e.g. curl 'https://www.google.com/voice/b/0/service/post' *** --data ****"
 read curl
 
 echo $curl > gv.txt
@@ -46,13 +46,25 @@ echo " "
 echo " "
 echo " "
 
-echo "Please input your slack incoming webhook:"
-read slack
+echo "Please input your slack incoming webhook, e.g. https://hooks.slack.com/services/***/***/****"
+read SLACK_WEBHOOK
 echo " "
 echo " "
 echo "${line}"
 echo "The Slack incoming webhook:"
-echo "${slack}"
+echo "${SLACK_WEBHOOK}"
+echo "${line}"
+
+echo " "
+echo " "
+echo " "
+echo "Please input your slack channel, e.g. private"
+read SLACK_CHANNEL
+echo " "
+echo " "
+echo "${line}"
+echo "The Slack Channel:"
+echo "${SLACK_CHANNEL}"
 echo "${line}"
 echo " "
 echo "Press Any Key to Start";
@@ -64,9 +76,9 @@ for (( i=1; i>0; i++ ))
     b='[[null,null,"There was an error with your request. Please try again."]]';
 	if [[ "$a" != "$b" ]];
     then
-        echo "Get the $gv may be Successful, Total $i times" >> result.txt
+        echo "Maybe Get the $gv Successful, Total $i times" >> result.txt
 	echo "${a}" >> result.txt
-        curl -X POST --data-urlencode "payload={\"channel\": \"#private\", \"username\": \"Notice\", \"text\": \"Google Voice may have been applied successfully.\", \"icon_emoji\": \":smile:\"}" "${slack}"
+        curl -X POST --data-urlencode "payload={\"channel\": \"#${SLACK_CHANNEL}\", \"username\": \"Notice\", \"text\": \"Total $i times, Google Voice may have been applied successfully.\", \"icon_emoji\": \":smile:\"}" "${SLACK_WEBHOOK}"
     else
         echo "$i times) ${a}"
 	fi
